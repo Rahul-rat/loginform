14642266b0114cf4c9a64adc467efd22e4c2ca16
Kalam <129867497+Rahul-rat@users.noreply.github.com>
2024-06-27 15:36:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+app = Flask('__name__')
+@app.route("/")
+def index() :
+  with open('index.html', 'r') as file : 
+    data = file.read()
+  return data
+app.runt(host="0.0.0.0", port=8080)
