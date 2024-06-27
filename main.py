2bc9451d3a82158b9b65def2726b170e3e2d3bb8
Kalam <129867497+Rahul-rat@users.noreply.github.com>
2024-06-27 15:40:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ def index() :
   with open('index.html', 'r') as file : 
     data = file.read()
   return data
+@app.route("/page") 
+def page() : 
+  return 'hello'
 app.runt(host="0.0.0.0", port=8080)
